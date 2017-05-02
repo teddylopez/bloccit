6b6da2c6d5f5e2afac2adc6c6e80ec7e8720ae7c
tedlopez <tedmlopez@yahoo.com>
2017-05-01 22:25:39 -0400

    Topics and posts -- Refactoring in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :questions
   resources :advertisements
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
-  root to: 'welcome#index'
+  root 'welcome#index'
 
   end
