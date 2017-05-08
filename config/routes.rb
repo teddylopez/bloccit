5e2a608cf2288a3b3af06ca9cf11f86d6ac88aed
tedlopez <tedmlopez@gmail.com>
2017-05-07 20:33:58 -0400

    Checkpoint complete: sign-in functionality created

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
