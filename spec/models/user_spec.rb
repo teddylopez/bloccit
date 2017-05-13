d70b8a92878c803e1abcb41f4381f63c00da2538
tedlopez <tedmlopez@yahoo.com>
2017-05-13 11:17:37 -0400

    Adding moderator role 1/2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,10 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:admin?)
     end
 
+    it 'should respond to moderator?' do
+      expect(user).to respond_to(:moderator?)
+    end
+
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
@@ -48,6 +52,10 @@ RSpec.describe User, type: :model do
       it "returns false for #admin?" do
         expect(user.admin?).to be_falsey
       end
+
+      it "returns false for #moderator?" do
+        expect(user.moderator?).to be_falsey
+      end
     end
 
     context "admin user" do
@@ -59,8 +67,30 @@ RSpec.describe User, type: :model do
         expect(user.member?).to be_falsey
       end
 
+      it 'should return false for #moderator?' do
+        expect(user.moderator?).to be_falsey
+      end
+
       it "returns true for #admin?" do
         expect(user.admin?).to be_truthy
+      end
+    end
+
+    context 'moderator user' do
+      before do
+        user.moderator!
+      end
+
+      it 'should return false for #member?' do
+        expect(user.member?).to be_falsey
+      end
+
+      it 'should return true for #moderator?' do
+        expect(user.moderator?).to be_truthy
+      end
+
+      it 'should return false for #admin?' do
+          expect(user.admin?).to be_falsey
       end
     end
   end
