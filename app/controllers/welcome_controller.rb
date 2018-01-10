5beba3cc6c073a1543ab5131af8a08f77b97b36b
teddylopez <tedmlopez@gmail.com>
2018-01-09 22:05:08 -0500

    Homepage style changes.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @posts = current_user.posts.all.limit(5).order("created_at desc")
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :posts, only: [] do
+  resources :posts do
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
 
